a2c60c2793fb34bf5139dc3db6c7eeb5affc007a
playsenge <40773107+playsenge@users.noreply.github.com>
2020-11-13 15:37:26 +0100

    Fresh commit

--- script.bat
+++ script.bat
@@ -7,9 +7,9 @@ echo Starting operation... (C:\Users\senge\Desktop\StoragePusher)
 echo.
 git init
 git lfs install
-git lfs track "*.png"
+git lfs track "C:\Users\senge\Desktop\StoragePusher/**"
 git add .gitattributes
 git add .
-git commit -m "sengeStorage Commit"
+git commit -m "Fresh commit"
 git pull https://github.com/playsenge/storage.git master
 git push https://github.com/playsenge/storage.git master
